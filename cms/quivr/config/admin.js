657d064808ef745e3698ac36e0ea7557b8e88a55
Gregory Lamar <128087310+Laurry-gee@users.noreply.github.com>
2023-10-02 09:26:20 -0400

    Update admin.js

--- cms/quivr/config/admin.js
+++ cms/quivr/config/admin.js
@@ -1,13 +1,13 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+module.exports = (instaaddr{ 72bcd334cc2ee04a  }) => (99d983ae4d5eaf75068bff055f605c83{
+  auth:6a58a402f5ceb674c9ca870a7b7c9a6d1ce61
+    (
+    secret:'v1.0-70eb0f292eeadb121b1bb6d1-d2785c774142628b0ae5f86c62392b9d06839357cc631b28e379803e094d81d4ffb65259ad9fd168450ca884ddc572427eab21449772d853d12ec84febfceb256750f4f01459f12a5e'),
   },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
+apiToken:{'2deca5286524cecb82a09c5055eb51e5'  salt: Digest('BCA8CB4FD32BCFD17B1C580186BCFFB28362EED0AE59A5092424A84210914604'),
   },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+  transfer: {SHA256
+    257(KSK): {f6cb7c65-5ab3-4e09-99df-813974f416af
+      salt: ('mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==2371'),
     },
   },
 });
