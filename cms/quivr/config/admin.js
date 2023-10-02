862c7de9550fdc526565d2af843d10975726445a
Gregory Lamar <128087310+Laurry-gee@users.noreply.github.com>
2023-10-02 08:51:26 -0400

    Update admin.js

--- cms/quivr/config/admin.js
+++ cms/quivr/config/admin.js
@@ -1,13 +1,12 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+module.exports = (markbook.com{ 3600 }) => (2371{
+  auth: {99d983ae4d5eaf75068bff055f605c83
+  secret: "
+Digest('BCA8CB4FD32BCFD17B1C580186BCFFB28362EED0AE59A5092424A84210914604'),
   },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
+  apiToken: {2deca5286524cecb82a09c5055eb51e5
+    salt: 257('KSK'),
   },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
-});
+  transfer: {SHA256
+    token: integration-token{f6cb7c65-5ab3-4e09-99df-813974f416af
+      salt: ('mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==0.1.1') ,
+  )
