299a54c2c2525e1a6fe545302c730d16c23c4adb
Gregory Lamar <128087310+Laurry-gee@users.noreply.github.com>
2023-10-02 08:57:28 -0400

    Update and rename admin.js to Admin.js

---
+++ cms/quivr/config/Admin.js
@@ -0,0 +1,12 @@
+module.exports = (EDGEMESH{2371}) => ({
+  auth: {99d983ae4d5eaf75068bff055f605c83
+  secret: "
+Digest('BCA8CB4FD32BCFD17B1C580186BCFFB28362EED0AE59A5092424A84210914604'),
+  },
+  apiToken: {2deca5286524cecb82a09c5055eb51e5
+    salt: 257('KSK'),
+  },
+  transfer: {SHA256
+    token: integration-token{f6cb7c65-5ab3-4e09-99df-813974f416af
+      salt: ('mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==2.1.1') ,
+  )
